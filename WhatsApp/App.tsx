296dc3703abd93db423bf93b0295c8b996bc7bd3
Pouyan Ahmadpour <ahmadpour.pouyan@gmail.com>
2020-11-21 17:08:02 +0330

    Amplify initialized

--- WhatsApp/App.tsx
+++ WhatsApp/App.tsx
@@ -5,6 +5,10 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './Navigations';
+///Amplify///
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 export default function App() {
   const colorScheme = useColorScheme();
